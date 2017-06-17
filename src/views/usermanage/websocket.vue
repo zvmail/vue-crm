2de3fb2ad0092a7dc89104511c683f560c4ccf61
vincent.zhang <zvmail@21cn.com>
2017-06-17 13:48:42 +0800

    断开时状态改变

--- src/views/usermanage/websocket.vue
+++ src/views/usermanage/websocket.vue
@@ -2,6 +2,7 @@
   <div class="app-container calendar-list-container">
     <h1>WebScoket Example</h1>
     <el-button type="primary" icon="document" @click="send"> Send </el-button>
+    <el-button type="primary" icon="document" @click="sendOne"> Send One</el-button>
     <div>
       <p v-if="isConnected">We're connected to the server!</p>
       <p>Message from server: "{{socketMessage}}"</p>
@@ -18,7 +19,8 @@ export default {
   data: () => ({
       stompClient: undefined,
       isConnected: false,
-      socketMessage: ''
+      socketMessage: '',
+      username: 'userA'
 
   }),
   methods: {
@@ -27,19 +29,39 @@ export default {
       console.log('send');
       this.stompClient.send('/app/hello', {}, JSON.stringify({ name: 'aaa' }));
     },
-    showGreeting(data) {
-         // (JSON.parse(greeting.body).content);
-         this.socketMessage = this.socketMessage + '\r\n' + data.body;
+    sendOne() {
+      // this.socket.emit('/app/hello', { name: 'aaa' });
+      console.log('send one');
+      this.stompClient.send('/app/message', {}, JSON.stringify({ name: 'bbb' }));
     }
   },
   created() {
-      const sock = new SockJS('http://localhost:8081/gs-guide-websocket');
+      // 利用option function()指定session-ID
+      const sock = new SockJS('http://localhost:8081/gs-guide-websocket'
+          , null, { sessionId: () => this.username });
+      // console.log(sock);
+
+      sock.onclose = function() {
+        this.isConnected = false;
+        console.log('close');
+      };
 
       this.stompClient = Stomp.over(sock);
-      this.stompClient.connect({}, frame => {
+
+      this.stompClient.connect({}, () => {
           this.isConnected = true;
-          console.log('Connected: ' + frame);
-          this.stompClient.subscribe('/topic/greetings', this.showGreeting);
+          //console.log('Connected: ' + frame);
+          this.stompClient.subscribe('/topic/greetings', data => {
+            // (JSON.parse(greeting.body).content);
+            this.socketMessage = this.socketMessage + '\r\n' + data.body;
+          });
+          this.stompClient.subscribe('/user/' + this.username + '/message', data => {
+            // (JSON.parse(greeting.body).content);
+            this.socketMessage = this.socketMessage + '\r\n' + data.body;
+          });
+      }, error => {
+        this.isConnected = false;
+        console.log(error);
       });
   }
 }
